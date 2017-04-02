3766678568d7c82a6eb823e13032dd2d46ca0436
famanoder <915905174@qq.com>
2017-04-02 18:37:48 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,26 +1,11 @@
-import {ObjectID} from 'BSON';
-export default function(router){
-	let tpl=`
-		<form method="POST" action="/action/fsfcf" enctype="multipart/form-data">
-			<input type="file" value="" name="user">	
-			<p><input type="submit" value="Submit"></p>
-		</form>
-		<form method="POST" action="/action" enctype="multipart/form-data">
-			<input type="file" value="" name="user">	
-			<p><input type="submit" value="Submit"></p>
-		</form>
-	`;
 
+export default function(router){
+	
 	router.get('/',async (cx,next)=>{
-		let doc=await cx.mongo.db('myBlog').collection('articals').find(_id:ObjectID('aecae8a3a3ceaf0a1')).toArray();
 		cx.type='text/html';
-		console.log(doc[0].content);
 		cx.response.body=`Hello Koa2 !`;
 	});
-	router.get('/:name',async (ctx,next)=>{
-		ctx.type='text/html';
-		ctx.response.body=tpl;
-	});
+
 	router.post('/action',async (cx,next)=>{
 		console.log(cx.request.body);
 		var d='';
