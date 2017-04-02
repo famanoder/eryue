22315ba3cbae3fd685c0187c43c8ceb24ad9843d
famanoder <915905174@qq.com>
2017-04-02 18:45:54 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,11 @@
 export default {
 	port:'9000',
-	staticDir:'./static'
+	staticDir:'./static',
+	mongoOption:{
+		user:'famanoder',
+		pass:'123321',
+		host:'127.0.0.1',
+		port:'1314',
+		db:'myBlog'
+	}
 }
